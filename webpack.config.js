3a6c1d47d4568e7e66dea683ea92a0b4d70e1f90
lenchevskii <dima.lenchevskii@gmail.com>
2020-12-10 15:56:54 +0300

    clean environment

--- webpack.config.js
+++ webpack.config.js
@@ -17,9 +17,9 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
-        // query: {
-        //   presets: ['es2015', 'react']
-        // }
+        options: {
+          presets: ["@babel/react", "@babel/env"]
+        }
       }
     ]
   },
