a6073431997e27e4d666a95ffa4cc0eb4a0c0f58
lenchevskii <dima.lenchevskii@gmail.com>
2020-12-26 14:09:06 +0300

    beautify

--- server/tests/instagram.worm.test.js
+++ server/tests/instagram.worm.test.js
@@ -1,20 +1,20 @@
-const { getInstagramCollection } 	= require('../utils/instagram.worm')
+const { getInstagramCollection }    = require('../utils/instagram.worm')
 
-const { head } 										= require('lodash/fp')
+const { head }                      = require('lodash/fp')
 
-const { PORT }										= require('dotenv').config().parsed
+const { PORT }                      = require('dotenv').config().parsed
 
-const multipleImageUrl 						= "https://www.instagram.com/p/B_xfntNJleV/"
+const multipleImageUrl              = "https://www.instagram.com/p/B_xfntNJleV/"
 
-const singleImageUrl 							= "https://www.instagram.com/p/Bo8e6FsgLlK/"
+const singleImageUrl                = "https://www.instagram.com/p/Bo8e6FsgLlK/"
 
-const videoImageMixedUrl					= "https://www.instagram.com/p/CGDADokpb1X/"
+const videoImageMixedUrl            = "https://www.instagram.com/p/CGDADokpb1X/"
 
-const singleVideoUrl							= "https://www.instagram.com/p/CIoa4oWFISU/"
+const singleVideoUrl                = "https://www.instagram.com/p/CIoa4oWFISU/"
 
-const localhost 									= `https://localhost:${PORT}/instagram-worm`
+const localhost                     = `https://localhost:${PORT}/instagram-worm`
 
-const realMoon 										= [
+const realMoon                      = [
 
 	{
 		"src": "https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/95715752_272475547130098_3584185748222201861_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=s4XlOHSH0dEAX-0hKuN&tp=1&oh=e0ec6feb5fdbcf7b8b308dce9d80c9c9&oe=600D6AF8",
@@ -43,7 +43,7 @@ const realMoon 										= [
 	}
 ]
 
-const realEngineer 								= [
+const realEngineer                  = [
 	{
 		"src": "https://scontent-waw1-1.cdninstagram.com/v/t51.2885-15/e35/43914853_950008155183946_9094264551367023346_n.jpg?_nc_ht=scontent-waw1-1.cdninstagram.com&_nc_cat=110&_nc_ohc=oTxiQGQDbb8AX-zKD_Y&tp=1&oh=640df227e9f2ee1f73fcd3780c79f919&oe=600ED05F",
 		"config_width": 1080,
@@ -51,41 +51,45 @@ const realEngineer 								= [
 	}
 ]
 
-const realNewbieSurprise					= []
+const realNewbieSurprise            = []
 
-const realLego										= []
+const realLego                      = []
 
-it(`Instagram. Steal Saturn's icy moon Mimas (multi-image post).  URL: ${multipleImageUrl}`, 		async () => {
+it(`Instagram. Steal Saturn's icy moon Mimas (multi-image post).  URL: ${multipleImageUrl}`,    async () => {
 
-	const stolenMoon = await getInstagramCollection(multipleImageUrl)
+	const stolenMoon                  = await getInstagramCollection(multipleImageUrl)
 
-	expect(stolenMoon.map(x => trimToken(x.src))).toEqual(realMoon.map(x => trimToken(x.src)))
+  expect(stolenMoon.map(x => trimToken(x.src)))
+  .toEqual(realMoon.map(x => trimToken(x.src)))
 })
 
-it(`Instagram. Steal Scandiweb engineer (single image post).      URL: ${singleImageUrl}`, 			async () => {
+it(`Instagram. Steal Scandiweb engineer (single image post).      URL: ${singleImageUrl}`,      async () => {
 	
-	const stolenEngineer = await getInstagramCollection(singleImageUrl)
+	const stolenEngineer              = await getInstagramCollection(singleImageUrl)
 
-	expect(stolenEngineer.map(x => trimToken(x.src))).toEqual(realEngineer.map(x => trimToken(x.src)))
+  expect(stolenEngineer.map(x => trimToken(x.src)))
+  .toEqual(realEngineer.map(x => trimToken(x.src)))
 })
 
-// it(`Instagram. Steal newbie' surprises (video & image post).      URL: ${videoImageMixedUrl}`, 	async () => {
+it(`Instagram. Steal newbie' surprises (video & image post).      URL: ${videoImageMixedUrl}`,  async () => {
 	
-// 	const stolenNewbieSurprise = await getInstagramCollection(videoImageMixedUrl)
+	const stolenNewbieSurprise        = await getInstagramCollection(videoImageMixedUrl)
 
-// 	expect(stolenNewbieSurprise.map(x => trimToken(x.src))).toEqual(realNewbieSurprise.map(x => trimToken(x.src)))
-// })
+  expect(stolenNewbieSurprise.map(x => trimToken(x.src)))
+  .toEqual(realNewbieSurprise.map(x => trimToken(x.src)))
+})
 
-// it(`Instagram. Steal Lego from a boy (single video post).         URL: ${singleVideoUrl}`, 			async () => {
+it(`Instagram. Steal Lego from a boy (single video post).         URL: ${singleVideoUrl}`,      async () => {
 	
-// 	const stolenLego = await getInstagramCollection(singleVideoUrl)
+	const stolenLego                  = await getInstagramCollection(singleVideoUrl)
 
-// 	expect(stolenLego.map(x => trimToken(x.src))).toEqual(realLego.map(x => trimToken(x.src)))
-// })
+  expect(stolenLego.map(x => trimToken(x.src)))
+  .toEqual(realLego.map(x => trimToken(x.src)))
+})
 
 
 // HELPERS
 
 // Trim time-dependent generated tokens (or whatever) from URL when the access was recieved
 
-const trimToken 									= (url) => head(url.match(/.*(_n\.jpg\?)/))
+const trimToken                     = (url) => head(url.match(/.*(_n\.jpg\?)/))
