98c466c91c5ee51a03d3f6dd9a9a70638818a934
lenchevskii <dima.lenchevskii@gmail.com>
2021-01-13 23:08:58 +0300

    add typewriter effect

--- client/app/components/Splash/Splash.js
+++ client/app/components/Splash/Splash.js
@@ -2,8 +2,13 @@ import styles                           from './splash.scss'
 import React                            from 'react'
 
 const Splash = ({Application}) => (
-  <div className={styles.container}>
-    <Application />
+  <div>
+    <div className={styles.container}>
+      <Application />
+    </div>
+    <div className={styles.greeting}>
+      welcom to the world my visual lover
+    </div>
   </div>
 )
 
